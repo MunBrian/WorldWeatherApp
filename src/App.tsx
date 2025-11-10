1e4ad3e61ca00cd699088bb15797e611bf676acb
MunBrian <bryanmungai254@gmail.com>
2025-11-10 23:18:50 +0300

    Initial app configurations and setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useQuery } from "@tanstack/react-query";
+import { getWeather } from "./api";
+
+function App() {
+  const { data } = useQuery({
+    queryKey: ["weather"],
+    queryFn: () => getWeather({ lat: 50, lon: 50 }),
+  });
+  return <div>{JSON.stringify(data)}</div>;
+}
+
+export default App;
---
+++ src/api.ts
@@ -0,0 +1,15 @@
+interface location {
+  lat: number;
+  lon: number;
+}
+
+const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+
+export async function getWeather({ lat, lon }: location) {
+  const res = await fetch(
+    `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,alerts&appid=${API_KEY}`
+  );
+  const data = await res.json();
+
+  return data;
+}
